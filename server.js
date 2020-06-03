99eaa371cd13e68189d494dfe948a8d995d60d61
Gurram, Thrinath R <thrinath_gurram@optum.com>
2020-06-03 12:40:47 +0530

    heroku changes

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const favicon = require("express-favicon");
-const path = require("path");
-const port = process.env.PORT || 8080;
+const express = require('express');
+const compression = require('compression');
+const path = require('path');
 const app = express();
-app.use(favicon(__dirname + "/build/favicon.ico"));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function (req, res) {
-  return res.send("pong");
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
 });
-console.log(port);
-app.listen(port);
